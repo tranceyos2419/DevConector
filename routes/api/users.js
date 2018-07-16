da8c62b0cdb86830622e8927136f963176aae7f5
tranceyos2419 <tranceyos2419@gmail.com>
2018-07-16 11:22:40 +0900

    Complete Section2

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// @Route  Get api/user/test
+// @desc   Tests users route
+// @access Public
+
+router.get('/test', (req, res) => res.json({
+    msg: `Users works`
+}));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users.js');
+const profile = require('./routes/api/profile.js');
+const posts = require('./routes/api/posts.js');
+
 const app = express();
 
 //* DB config
@@ -14,6 +18,12 @@ mongoose.connect(db, {
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello world'));
+
+//* Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 
 const port = process.env.PORT || 5000;
 
